79cdb6ce170f19918f253c6beed483368b6e1ca0
abhi-kr07 <abhishekkr98dhn@gmail.com>
2025-02-26 14:56:57 +0530

    added one more method for user-management

--- user_management/user_management.sh
+++ user_management/user_management.sh
@@ -8,3 +8,21 @@ do
         sudo useradd ${users}
         echo "${users}:${PASS}" | sudo chpasswd
 done
+
+#!/bin/bash
+#
+
+echo "Type the username you want to add"
+read username
+echo "Enter the password you want to set for $username"
+read -s password
+if ! getent passwd "$username";
+then
+        echo "Going to add $username"
+        sudo useradd $username
+        echo "$username:$password" | sudo chpasswd
+        echo -e "\033[0;32m user added sucessfully \033[0m"
+else
+        echo "user is present already!!"
+fi
+
